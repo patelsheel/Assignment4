151bcd4af63448c8b11140178612b15f6d6b9296
patelsheel <77370692+patelsheel@users.noreply.github.com>
2023-09-15 23:59:30 -0400

    Initial commit

---
+++ StoreDatabase/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreDatabase.Entities
+{
+    [Table("Orders")]
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerID { get; set; }
+
+        public Customer? Customer { get; set; } = null;
+        public virtual ICollection<OrderLineItem>
+            OrderLineItems
+        { get; set; } = new List<OrderLineItem>();
+    }
+}
+
